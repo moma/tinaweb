5c522bc4d45b5c72f686b8a6fc50bc2459334ca6
elishowk <elishowk@nonutc.fr>
2010-05-11 18:38:57 +0200

    right and left click events implemented

--- html/js/tinaviz.js
+++ html/js/tinaviz.js
@@ -50,12 +50,11 @@ function displayInfodivTagCloud( id ) {
 /*
  * updates the infodiv contents
  */
-function displayInfodiv( level, id, label, attr, mouse ) {
+function displayInfodiv( level, id, label, attr ) {
     var nodelabel = $( "#node_label" );
     nodelabel.empty().html( "<h2>"+label+"</h2>" );
     var contents = $( "#node_contents" );
     contents.empty();
-    attr = $.getJSON( attr );
     if ( attr.category == 'NGram' ) {
         // do not display nothing
     }
@@ -199,8 +198,11 @@ function Tinaviz() {
             if (applet == null) return;
             return applet.getView(level).getProperty(key);
         }
-        this.search= function(txt) {
-            this.logNormal("Searching is not implemented yet..");
+        this.getNodesByLabel= function(label, search_type) {
+            if (applet == null || search_type != "equals" || search_type != "contains"|| search_type != "startsWith" || search_type != "endsWith" || search_type != "equalsIgnoreCase") {
+                return;
+            }
+            return applet.getNodesByLabel(label, search_type);
         }
         this.unselect= function() {
             if (applet != null)  applet.unselect();
@@ -220,22 +222,38 @@ function Tinaviz() {
             alert( applet.getNeighbourhood(id) );
             return $.parseJSON( applet.getNeighbourhood(id) );
         }
-        this.nodeSelected = function(level,x,y,id,label,attr,mouse) {
-            if (mouse == "left")
-                return displayInfodiv( level, id, label, attr, mouse );
+        this.nodeLeftClicked = function(level, x, y, id, label, attr) {
+            if ( attr == null ) return;
+            return displayInfodiv( level, id, label, attr);
         }
-        this.enabled= function() {
+        this.nodeRightClicked = function(level, x, y, id, label, attr) {
+            if (applet == null) return;
+            var cat = this.getProperty(level, "category/value");
+            if (cat == "Document") newcategory = "NGram";
+            if (cat == "NGram") newcategory = "Document";
+            this.setProperty("macro", "category/value", newcategory);
+            this.touch(level);
+            this.recenter();
+        }
+        this.nodeSelected = function(level, x, y, id, label, attr, mouse) {
+            if ( mouse == "left" ) {
+                this.nodeLeftClicked(level,x,y,id,label,$.parseJSON(attr));
+            } else if ( mouse == "right" ) {
+                this.nodeRightClicked(level,x,y,id,label,$.parseJSON(attr));
+            }
+        }
+        this.enabled = function() {
             if (applet == null) {
                 return false;
             } else {
                 return applet.isEnabled();
             }
         }
-        this.enable=  function() {
+        this.enable =  function() {
             if (applet == null) return;
             applet.setEnabled(true);
         }
-        this.disable=  function() {
+        this.disable =  function() {
             if (applet == null) return;
             applet.setEnabled(false);
         }
