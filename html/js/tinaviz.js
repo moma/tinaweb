f1fbec763657c2805c6df6c52b93ccc2963cf083
Julian Bilcke <julian.bilcke@iscpif.fr>
2010-06-16 19:43:41 +0200

    changed the way edges are displayed

--- html/js/tinaviz.js
+++ html/js/tinaviz.js
@@ -385,7 +385,7 @@ function Tinaviz() {
 
             this.bindFilter("NodeWeightRange", "nodeWeight", "macro");
             this.bindFilter("EdgeWeightRange", "edgeWeight", "macro");
-            
+
             this.bindFilter("NodeFunction", "radiusByWeight", "macro");
 
             this.bindFilter("Output", "output", "macro");
