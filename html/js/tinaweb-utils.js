5c8906f66429e77fd8337a9e66a7f13caa35d40d
Julian Bilcke <julian.bilcke@iscpif.fr>
2010-07-13 16:39:49 +0200

    new feature: URL parameters

--- html/js/tinaweb-utils.js
+++ html/js/tinaweb-utils.js
@@ -93,3 +93,16 @@ var resize = function() {
         $("#infodivparent").css('height', size.h);
         return size;
 };
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
