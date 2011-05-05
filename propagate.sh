c0201e5ec6e5c45087d6beb103ae4cd7d3b0688e
Julian Bilcke <julian.bilcke@iscpif.fr>
2011-05-05 12:12:39 +0200

    updated propagater

--- propagate.sh
+++ propagate.sh
@@ -5,4 +5,7 @@ git push origin whoswho
 git checkout scholars
 git pull origin master
 git push origin scholars
+git checkout fet70
+git pull origin master
+git push origin fet70
 git checkout master
