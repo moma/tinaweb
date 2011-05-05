fb40653c55a187b4e1c597ee0c151114945d489a
Julian Bilcke <julian.bilcke@iscpif.fr>
2011-05-05 12:14:33 +0200

    updated master

--- propagate.sh
+++ propagate.sh
@@ -5,7 +5,7 @@ git push origin whoswho
 git checkout scholars
 git pull origin master
 git push origin scholars
-git checkout fet70
+git checkout fetopen
 git pull origin master
-git push origin fet70
+git push origin fetopen
 git checkout master
