aa062cd21b0746cb78a9b88bb6808fde59ff0c37
David Chavalarias <david.chavalarias@polytechnique.edu>
2011-05-13 08:57:47 +0200

    saving

--- js/preferences.js
+++ js/preferences.js
@@ -5,14 +5,8 @@ var prefs = {
             category: "Document",
             node_id: "",
             search: "",
-
-<<<<<<< HEAD
-            a_node_size: "0.10",
-            b_node_size: "0.10",
-=======
             a_node_size: "0.50",
             b_node_size: "0.50",
->>>>>>> 35d6ebc266798e288bbfa50b5430f8235debcbd0
 
             cursor_size: "0.01",
 
