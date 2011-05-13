156ebaa50f1b4cdc8034296d9c52099a7a61aef5
David Chavalarias <david.chavalarias@polytechnique.edu>
2011-05-13 10:22:50 +0200

    vimeo

--- js/tinaweb-info.js
+++ js/tinaweb-info.js
@@ -323,7 +323,7 @@ var InfoDiv = {
         }
 
         this.unselect_button.hide();
-        this.label.empty().append($("<h2></h2>").html("<center><iframe src='http://player.vimeo.com/video/13203336' width='300' height='210' frameborder='0'></iframe><\center>"));
+        this.label.empty().append($("<h2></h2>").html("<center><iframe src='http://player.vimeo.com/video/21919900' width='300' height='210' frameborder='0'></iframe><\center>"));
 
         this.contents.empty().append($("<h4></h4>").html("click on a node to begin exploration"));
         this.contents.empty().append($("<div><div>").html(
