7e1a3c1cde2e32b9d0d5a8c35532b5d692c65707
David Chavalarias <david.chavalarias@polytechnique.edu>
2012-02-11 10:09:33 +0100

    directory improved

--- directory.php
+++ directory.php
@@ -1,7 +1,8 @@
 <?php
+
 include ("parametres.php");
 
-echo '<!DOCTYPE html>
+$content = '<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -30,7 +31,7 @@ echo '<!DOCTYPE html>
         <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
     </head>
     <body>
-        <script type="text/javascript" src="js/whoswho.js"/></script>
+        <script type="text/javascript" src="js/whoswho.js"></script>
     <div class="container-fluid">
         <div id="visualization"></div>
         <!-- Main hero unit for a primary marketing message or call to action -->
@@ -43,179 +44,184 @@ echo '<!DOCTYPE html>
 $base = new PDO("sqlite:" . $dbname);
 $termsMatrix = array(); // liste des termes présents chez les scholars avec leurs cooc avec les autres termes
 $scholarsMatrix = array(); // liste des scholars avec leurs cooc avec les autres termes
-$scholarsIncluded=0;
+$scholarsIncluded = 0;
 
 
-echo '<div id="welcome">
-						<h2>Complex Systems Directory</h2>.
+$content .= '<div id="welcome">
+<img src="img/RegistryBanner.png" align="center">						
+<h2>Complex Systems Directory</h2>.
 						<p>
                                                 This is the list of scholars
 						</p>
-						</p>
+						
 						
             </div>';
 
 // liste des chercheurs
-$sql = "SELECT * FROM scholars ";
+$sql = "SELECT * FROM scholars where country='France' ORDER BY last_name";
 
 $scholars = array();
 //$query = "SELECT * FROM scholars";
 foreach ($base->query($sql) as $row) {
-$info = array();
-$info['unique_id'] = $row['unique_id'];
-$info['first_name'] = $row['first_name'];
-$info['initials'] = $row['initials'];
-$info['last_name'] = $row['last_name'];
-$info['nb_keywords'] = $row['nb_keywords'];
-$info['css_voter'] = $row['css_voter'];
-$info['css_member'] = $row['css_member'];
-$info['keywords_ids'] = explode(',', $row['keywords_ids']);
-$info['keywords'] = $row['keywords'];
-$info['status'] = $row['status'];
-$info['country'] = $row['country'];
-$info['homepage'] = $row['homepage'];
-$info['lab'] = $row['lab'];
-$info['affiliation'] = $row['affiliation'];
-$info['lab2'] = $row['lab2'];
-$info['affiliation2'] = $row['affiliation2'];
-$info['homepage'] = $row['homepage'];
-$info['title'] = $row['title'];
-$info['position'] = $row['position'];
-$info['photo_url']=$row['photo_url'];
-$info['interests']=$row['interests'];
-$info['address']=$row['address'];
-$info['city']=$row['city'];
-$info['postal_code']=$row['postal_code'];
-$info['phone']=$row['phone'];
-$info['mobile']=$row['mobile'];
-$info['fax']=$row['fax'];
-$info['affiliation_acronym']=$row['affiliation_acronym'];
-$scholars[$row['unique_id']] = $info;
+    $info = array();
+    $info['unique_id'] = $row['unique_id'];
+    $info['first_name'] = $row['first_name'];
+    $info['initials'] = $row['initials'];
+    $info['last_name'] = $row['last_name'];
+    $info['nb_keywords'] = $row['nb_keywords'];
+    $info['css_voter'] = $row['css_voter'];
+    $info['css_member'] = $row['css_member'];
+    $info['keywords_ids'] = explode(',', $row['keywords_ids']);
+    $info['keywords'] = $row['keywords'];
+    $info['status'] = $row['status'];
+    $info['country'] = $row['country'];
+    $info['homepage'] = $row['homepage'];
+    $info['lab'] = $row['lab'];
+    $info['affiliation'] = $row['affiliation'];
+    $info['lab2'] = $row['lab2'];
+    $info['affiliation2'] = $row['affiliation2'];
+    $info['homepage'] = $row['homepage'];
+    $info['title'] = $row['title'];
+    $info['position'] = $row['position'];
+    $info['photo_url'] = $row['photo_url'];
+    $info['interests'] = $row['interests'];
+    $info['address'] = $row['address'];
+    $info['city'] = $row['city'];
+    $info['postal_code'] = $row['postal_code'];
+    $info['phone'] = $row['phone'];
+    $info['mobile'] = $row['mobile'];
+    $info['fax'] = $row['fax'];
+    $info['affiliation_acronym'] = $row['affiliation_acronym'];
+    $scholars[$row['unique_id']] = $info;
 }
-$imsize=150;
+$imsize = 150;
 
 
 // ajout des scholars
 foreach ($scholars as $scholar) {
-    
-echo '<div class="row">
+
+    $content .= '<div class="row">
                 <div class="span12">                    
                     <div class="row">           
                         <div class="span9" align="justify">';
+    $content .= '<div>';
+    if ($scholar['photo_url'] != null) {
+        $content .= '<img style="margin: 7px 10px 10px 0px" src="http://main.csregistry.org/' . $scholar['photo_url'] . '" width="' . $imsize . 'px" align="left">';
+    }
 
-if ($scholar['photo_url']!=null){
-echo '<img style="margin: 7px 10px 10px 0px" src="http://main.csregistry.org/'.$scholar['photo_url'].'" width="'.$imsize.'px" align="left">';
-}
-
-echo '<h2 >'.$scholar['title']. ' ' . $scholar['first_name'] . ' ' . $scholar['initials'] . ' ' . $scholar['last_name'].
-' <small> - '.$scholar['country'].'</small></h2>';
-
-
-if ($scholar['position']!=null){
-echo '<dl>
-<dt>'.$scholar['position'].'</dt>';
-}
-$affiliation='';
-if ($scholar['lab']!=null){
-$affiliation.=$scholar['lab'].',';
-}
-if ($scholar['affiliation']!=null){
-$affiliation.=$scholar['affiliation'];
-}
-if(($scholar['affiliation']!=null)|($scholar['lab']!=null)){
-echo '<dd>'.$affiliation.'</dd> ';
-}
+    $content .= '<h2 >' . $scholar['title'] . ' ' . $scholar['first_name'] . ' ' . $scholar['initials'] . ' ' . $scholar['last_name'] .
+            ' <small> - ' . $scholar['country'] . '</small></h2>';
 
 
-$affiliation2='';
-if ($scholar['lab2']!=null){
-$affiliation2.=$scholar['lab2'].',';
-}
-if ($scholar['affiliation2']!=null){
-$affiliation2.=$scholar['affiliation2'];
-}
-if(($scholar['affiliation2']!=null)|($scholar['lab2']!=null)){
-echo '<dd><i>Second affiliation :</i>'.$affiliation2.'</dd>';
-}
+    if ($scholar['position'] != null) {
+        $content .= '<dl>
+<dt>' . $scholar['position'] . '</dt>';
+    }
+    $affiliation = '';
+    if ($scholar['lab'] != null) {
+        $affiliation.=$scholar['lab'] . ',';
+    }
+    if ($scholar['affiliation'] != null) {
+        $affiliation.=$scholar['affiliation'];
+    }
+    if (($scholar['affiliation'] != null) | ($scholar['lab'] != null)) {
+        $content .= '<dd>' . $affiliation . '</dd> ';
+    }
 
-if ((strcmp($affiliation2, '')!=0)|(strcmp($affiliation, '')!=0)){
-echo '<br/>';
-}
 
-$www='';
-if (substr($scholar['homepage'], 0, 3)==='www'){
-$www.='<a href='.str_replace('&', ' and ', 'http://'.trim($scholar['homepage'])).' target=blank > '.str_replace('&', ' and ', 'http://'.trim($scholar['homepage'])).'  </a ><br/>';
-}elseif(substr($scholar['homepage'], 0, 4)==='http'){
-$www.='<a href='.str_replace('&', ' and ', trim($scholar['homepage'])).' target=blank > '.str_replace('&', ' and ', trim($scholar['homepage'])).' </a ><br/>';
-}
+    $affiliation2 = '';
+    if ($scholar['lab2'] != null) {
+        $affiliation2.=$scholar['lab2'] . ',';
+    }
+    if ($scholar['affiliation2'] != null) {
+        $affiliation2.=$scholar['affiliation2'];
+    }
+    if (($scholar['affiliation2'] != null) | ($scholar['lab2'] != null)) {
+        $content .= '<dd><i>Second affiliation :</i>' . $affiliation2 . '</dd>';
+    }
 
-if (strcmp($www, '')!=0){
-echo '<dd><i class="icon-home"></i>'.$www.'</dd> ';
-}
+    if ((strcmp($affiliation2, '') != 0) | (strcmp($affiliation, '') != 0)) {
+        $content .= '<br/>';
+    }
 
-if ($scholar['css_member']==='Yes'){
-if ($scholar['css_voter']==='Yes'){
-echo '<dd><i class="icon-user"></i> CSS Member premium</dd> ';
-}else{
-echo '<dd><i class="icon-user"></i> CSS Member</dd> ';
-}
-$scholar_desc.='<b>CSS Member </b>';
-}
+    $www = '';
+    if (substr($scholar['homepage'], 0, 3) === 'www') {
+        $www.=' <a href=' . trim(str_replace('&', ' and ', 'http://' . $scholar['homepage'])) . ' target=blank > ' . trim(str_replace('&', ' and ', 'http://' . $scholar['homepage'])) . '  </a ><br/>';
+    } elseif (substr($scholar['homepage'], 0, 4) === 'http') {
+        $www.=' <a href=' . trim(str_replace('&', ' and ', $scholar['homepage'])) . ' target=blank > ' . trim(str_replace('&', ' and ', $scholar['homepage'])) . ' </a ><br/>';
+    }
 
-if ($scholar['position']!=null){
-    echo '</dl>';
-}
+    if (strcmp($www, '') != 0) {
+        $content .= '<dd><i class="icon-home"></i>' . $www . '</dd> ';
+    }
 
-echo '</div>';
+    if ($scholar['css_member'] === 'Yes') {
+        if ($scholar['css_voter'] === 'Yes') {
+            $content .= '<dd><i class="icon-user"></i> CSS Member premium</dd> ';
+        } else {
+            $content .= '<dd><i class="icon-user"></i> CSS Member</dd> ';
+        }
+        $scholar_desc.='<b>CSS Member </b>';
+    }
 
-if ($scholar['keywords']!=null){
-echo '<div class="span3" align="justify">
-                 <i class="icon-tags"></i> '.$scholar['keywords'];
-echo '</div>';
-}
+    if ($scholar['position'] != null) {
+        $content .= '</dl>';
+    }
 
-echo '</div>';  
+    $content .= '</div>';
 
-echo '<div class="row">';           
-if ($scholar['interests']!=null){
-    echo '<h4 class="span9" align="justify">Research</h4>';
-    echo '<p class="span9" align="justify">'.$scholar['interests'].'</p>';
-}else{
-    echo '<p class="span9" align="justify"></p>';
-}         
 
-if (($scholar['address']!=null)||($scholar['phone']!=null)){
-    echo '<div class="span3" align="justify">';
-}
-if ($scholar['address']!=null){
-    echo '<address><i class="icon-envelope"></i> '.$scholar['address'].'<br/>'.$scholar['city'].'<br/>'.$scholar['postal_code'].'<br/></address>';
-     
-}
+    if ($scholar['interests'] != null) {
+        $content .= '<div>';
+        $content .= '<h4>Research</h4>';
+        $content .= '<p>' . $scholar['interests'] . '</p>';
+        $content .= '</div>';
+    }
 
-if ($scholar['phone']!=null){
-    echo '<address><strong>Phone</strong><br/>'.
-          $scholar['phone'].'<br/>'.$scholar['mobile'].'<br/>'.$scholar['fax'].'<br/></address>';
-}
+    $content .= '</div>';
 
+    if (($scholar['keywords'] != null) || ($scholar['address'] != null) || ($scholar['phone'] != null)) {
+        $content .= '<div class="span3" align="justify">';
+        
+        if ($scholar['keywords'] != null){
+                 $content .= '<i class="icon-tags"></i> ' . $scholar['keywords']. '<br/><br/>';
+        }
             
-if (($scholar['address']!=null)||($scholar['phone']!=null)){
-    echo '</div>';
-}
+        if ($scholar['address'] != null) {
+            $content .= '<address><i class="icon-envelope"></i> ' . $scholar['address'] . '<br/>' . $scholar['city'] . '<br/>' . $scholar['postal_code'] . '<br/></address>';
+        }
 
-echo '</div>';
-echo '</div>';
-echo '</div>';
-echo '<br/><br/><br/>';  
+
+        if ($scholar['phone'] != null) {
+            $content .= '<address><strong>Phone</strong>: '.$scholar['phone'] . '<br/>';
+            if ($scholar['mobile'] != null) {
+                $content .='<strong>Mobile</strong>: '.$scholar['mobile']. '<br/>';
+            }
+            if ($scholar['fax'] != null) {
+                $content .='<strong>Fax</strong>: '.$scholar['fax'] . '<br/>';
+            }            
+        }
+
+        $content .= '</div>';
+    }
+
+$content .= '</div>';
+
+    $content .= '</div>';
+    $content .= '</div>';
+    $content .= '<br/>';
+    $content .= '<br/>';
+    $content .= '<br/>';
     // fin du profil
-
 }
-echo '</div>';
-echo '</div>
+$content .= '</div>';
+$content .= '</div>
             <footer>
-                <a href="http://iscpif.fr"><img src="css/branding/logo-iscpif_medium.png" alt="iscpif.fr" style="border: none; margin-bottom : -6px;" title="isc-pif" /></a>- &copy; <a href="http://moma.csregistry.org" target="_BLANK">MOMA</a> - <a href="http://www.crea.polytechnique.fr/LeCREA/" target="_BLANK">CREA</a> - <a href="http://www.cnrs.fr/fr/recherche/index.htm" target="_BLANK">CNRS</a> - 2009-2012
+                GENERATED BY <a href="http://iscpif.fr"><img src="css/branding/logo-iscpif_medium.png" alt="iscpif.fr" style="border: none; margin-bottom : -6px;" title="isc-pif" /></a>-  <a href="http://sciencemapping.com" target="_BLANK">MOMA</a> - <a href="http://www.crea.polytechnique.fr/LeCREA/" target="_BLANK">CREA</a> - <a href="http://www.cnrs.fr/fr/recherche/index.htm" target="_BLANK">CNRS</a> 
             </footer>
         </div>
 </body>
-</html>'
+</html>';
 
+echo $content;
 ?>
