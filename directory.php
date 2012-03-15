33848c1b5c0e0063f258bb8519b3c22221be5af1
Julian bilcke <julian.bilcke@gmail.com>
2012-03-15 14:53:50 +0100

    added google analytics

--- directory.php
+++ directory.php
@@ -29,6 +29,18 @@ $meta = '<!DOCTYPE html>
         <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
         <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
         <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push([\'_setAccount\', \'UA-30062222-1\']);
+          _gaq.push([\'_setDomainName\', \'communityexplorer.org\']);
+          _gaq.push([\'_trackPageview\']);
+
+          (function() {
+            var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+            ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+            var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
     </head>
     <body>
         <script type="text/javascript" src="js/whoswho.js"></script>
